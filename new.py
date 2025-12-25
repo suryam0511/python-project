39774722df9bbde1cd0e42df81abb0bd959b90ba
SuryaPrakash <surya@aerem.co>
2025-12-25 18:56:20 +0530

    Initial commit

---
+++ new.py
@@ -0,0 +1,37 @@
+name = "Sathya"
+
+# print(name)
+# print(hello sathya, whats your age)
+
+# print("hello", name, "what is your age")
+# print(f"hello {name} what is your age")
+
+# name = (input("Enter your name: "))
+# age = (input("Enter your age: "))
+
+# print(f"Hello {name}, you are {age} years old.")
+
+# item = input("Enter the item you want to buy: ")
+# price = float(input("Enter the price of the item: "))
+# quantity = int(input("How many items do you want to buy: "))
+# total = price * quantity
+
+# print(f"You have bought {quantity} x {item}/s")
+# print(f"Your total is: &{total}")
+
+# # Mad Libs Game
+# adjective1 = input("Enter an adjective: ")
+# adjective2 = input("Enter another adjective: ")
+# noun1 = input("Enter a noun: ")
+# verb1 = input("Enter a verb: ")
+# adjective3 = input("Enter one more adjective: ")
+
+# print(f"The {adjective1} fox {verb1} over the {adjective2} dog.")
+# print(f"The {noun1} is very {adjective3}.")     
+
+import math
+x = 16
+
+result = math.sqrt(x)
+
+print(result) 
