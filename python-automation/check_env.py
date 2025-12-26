3ab740932411e3e34ec5a762f9846c7ede815a73
SuryaPrakash <surya@aerem.co>
2025-12-26 14:49:51 +0530

    TWS commit

---
+++ python-automation/check_env.py
@@ -0,0 +1,12 @@
+#get the environment from user 
+
+env = input("Enter the environment = ")
+
+print("The user input enc is",env)
+
+
+#simple arithmetic operations
+a = int(input("Enter first number = "))
+b = int(input("Enter second number = "))
+print("Multiplication:",a*b)
+
